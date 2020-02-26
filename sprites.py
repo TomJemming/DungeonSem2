7e636a3743ebef9039cd0e704e0c0211e76c10b6
admstudent <admstudent@NB-SCIE-06.alredu.alr.lu>
2020-02-26 12:09:03 +0100

    list of variables to include

--- sprites.py
+++ sprites.py
@@ -4,9 +4,18 @@ import math
 
 class Enemy(Sprite):
 
-    def __init__(self):
-        pass
+    def __init__(self, name, hp, move, evasion_x, evasion_y, evasion_cooldown):
+        self.hp = 1
+        self.name = name
+        if self.name = "spider":
+            self.move = 2
+        else:
+            self.move = 1
+        self.evasion_x = 0          #replace global values x2 and x4 to get local values
+        self.evasion_y = 0
+        self.evasion_cooldown = 0
+
 
     def damage(self):
-        if self.enemy_life == 0:
+        if self.hp == 0:
             enemy.kill
